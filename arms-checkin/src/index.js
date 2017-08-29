042db14e52c85456e02b565f4ba9cac76ff43bd7
poposang <po.b@arms-thai.com>
2017-08-29 11:00:03 +0700

    update file

--- arms-checkin/src/index.js
+++ arms-checkin/src/index.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import ReactDOM  from 'react-dom';
 import classes from './index.css';
-function tick() {
+import moment from 'moment';
 
+function time() {
+    // time = new Date().toLocaleTimeString();
+    // return time;
+}
+function tick() {
     const element = (
      <div>
         {/* <div className="fix-top">
             <img class="" src="https://facebook.github.io/react/img/logo.svg" height="50"/>
         </div> */}
-        <div className="container border">
-            <div className="text-center"><h2>Time Now  <br/>{new Date().toLocaleTimeString()}.</h2></div>
-            <input type="text"  className="form-control" placeholder="Enter Fast Code"/> 
+        <div className="container">
+            <div className="text-center">
+                <h2>Time Now  <br/> 
+                </h2></div>
+            <input type="text"  className="form-control mb-2" placeholder="Enter Fast Code"/> 
             <button type="button" className="btn btn-primary btn-block">Fast Checkin</button>
         
         </div>
