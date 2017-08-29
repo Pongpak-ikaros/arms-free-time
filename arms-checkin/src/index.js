26180de1e77a95cba409c99d5aae32f4fd57ccc5
poposang <po.b@arms-thai.com>
2017-08-29 09:55:01 +0700

    update file

--- arms-checkin/src/index.js
+++ arms-checkin/src/index.js
@@ -2,18 +2,27 @@ import React from 'react';
 import ReactDOM  from 'react-dom';
 import classes from './index.css';
 function tick() {
+
     const element = (
-      <div className="container">
-        <h2>It is {new Date().toLocaleTimeString()}.</h2>
-        <input type="text"  placeholder="Enter Fast Code"/>
-        <button class="btn"></button>
-      </div>
+     <div>
+        {/* <div className="fix-top">
+            <img class="" src="https://facebook.github.io/react/img/logo.svg" height="50"/>
+        </div> */}
+        <div className="container border">
+            <div className="text-center"><h2>Time Now  <br/>{new Date().toLocaleTimeString()}.</h2></div>
+            <input type="text"  className="form-control" placeholder="Enter Fast Code"/> 
+            <button type="button" className="btn btn-primary btn-block">Fast Checkin</button>
+        
+        </div>
+    </div>
     );
     ReactDOM.render(
       element,
       document.getElementById('root')
     );
   }
-  
+  function alertX(){
+    //   var x = require('moment');
+  }
 setInterval(tick, 10);
   
